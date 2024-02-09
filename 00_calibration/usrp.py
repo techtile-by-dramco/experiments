2fad520f770201a4f9b8c5ba838507cf3ad339a1
Gilles Callebaut, PhD <callebaut.gilles@gmail.com>
2024-02-09 14:31:36 +0100

    Update usrp.py

--- 00_calibration/usrp.py
+++ 00_calibration/usrp.py
@@ -81,10 +81,11 @@ def rx_ref(usrp, rx_streamer, quit_event, phase_to_compensate):
     # https://files.ettus.com/manual/page_sync.html#sync_phase_cordics
     # The CORDICs are reset at each start-of-burst command, so users should ensure that every start-of-burst also has a time spec set.
 
-    iq_data = np.empty((num_channels, int(DURATION*RATE*1.5)), dtype=np.complex64)
-    # Make a receive buffer
     num_channels = rx_streamer.get_num_channels()
     max_samps_per_packet = rx_streamer.get_max_num_samps()
+    iq_data = np.empty((num_channels, int(DURATION*RATE*1.5)), dtype=np.complex64)
+    # Make a receive buffer
+    
     # TODO: The C++ code uses rx_cpu type here. Do we want to use that to set dtype?
     recv_buffer = np.zeros((num_channels, 1000*max_samps_per_packet), dtype=np.complex64)
     rx_md = uhd.types.RXMetadata()
