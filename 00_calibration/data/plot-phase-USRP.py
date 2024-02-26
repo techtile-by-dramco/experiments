c505fbec838a9f319f5bb47c437ba1f990f83d9b
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-02-26 16:29:28 +0100

    update figs

--- 00_calibration/data/plot-phase-USRP.py
+++ 00_calibration/data/plot-phase-USRP.py
@@ -3,10 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import re
-from scipy.stats import norm
+from scipy.stats import norm, circmean, circstd
 
+import threading
 from datetime import datetime, timedelta
 from matplotlib.dates import HourLocator, MinuteLocator
+
+import allantools
+#TODO CHECK ALLAN DEV
 
 # Define the pattern to match the file names
 file_pattern0 = 'received_data_CH0_ALL'
@@ -22,6 +26,13 @@ file_paths = [f for f in os.listdir(script_dir) if os.path.isfile(os.path.join(s
 
 print(file_paths)
 
+# Define a function to be the target for each thread
+def circmean_thread(thread_id, results, val):
+    results[thread_id] = my_circmean(val)
+
+def my_circmean(phases):
+    return np.angle(np.sum(np.exp(phases*1j))) # circular mean https://en.wikipedia.org/wiki/Circular_mean
+
 # Iterate over each file and read its contents as np.float32
 for file_path in file_paths:
     # Construct the full path to the file
@@ -29,6 +40,8 @@ for file_path in file_paths:
 
     # Read the binary file and cast to np.float32
     data = np.fromfile(file_path, dtype=np.float32)
+    
+    print(f"Num samples: {len(data)}")
 
     # avg_phase = np.rad2deg(data[::2])
     # std_phase = np.rad2deg(data[1::2])
@@ -51,22 +64,65 @@ for file_path in file_paths:
     
     plt.plot(bins[:-1], hist)
     
-    
-    
-    
-    
+
     # Plot the PDF.
     xmin, xmax = plt.xlim()
     mu, std = norm.fit(data) 
     x = np.linspace(xmin, xmax, 1000)
     p = norm.pdf(x, mu, std)
-    label = "Fit Values: mu={:.2f} and std={:.2f}".format(mu, std)
+    label = f"Fit Values: mu={mu:.2f} and std={mu:.2f}"
     plt.plot(x, p, 'k', linewidth=2, label=label)
     
 
     plt.legend()
-    plt.savefig(file_path[:-4]+".png")
+    plt.savefig(file_path[:-4]+"-hist.png")
     plt.show()
+    
+    plt.figure()
+    plt.title(file_path)
+    
+    phase = np.deg2rad(data)
+    
+    # sample rate is 1e6
+    rate = 1e6
+    
+    sample_size = int(1e6)
+    
+
+    phase = np.array_split(phase, len(phase) / sample_size)
+    print("split done")
+    num_frames = len(phase)
+    
+    # means = np.zeros(num_frames)
+    # stds =  np.zeros(num_frames)
+    
+    means = np.array([my_circmean(phase[i]) for i in range(num_frames)])
+    
+    
+    
+    
+    # threads = []
+    # means = [None] * num_frames
+    # for i in range(num_frames):
+    #     th = threading.Thread(target=circmean_thread, args=(i, means, phase[i]))
+    #     th.start()
+    #     threads.append(th)
+        
+    # # Wait for all threads to finish
+    # for thread in threads:
+    #     thread.join()
+
+    
+    print("means done")
+    stds =  np.array([circstd(phase[i]) for i in  range(num_frames)])
+    print("stds done")
+    
+    # for i in range(num_frames):
+    #     means[i] = circmean(phase[i])
+    #     stds[i]  = circstd(phase[i])
+    
+    x = np.array(range(num_frames)) * (sample_size/rate) # in seconds  60 seconds
+    
     
     # # Now you can work with your data as np.float32 array
     # milli_seconds_array = np.arange(len(avg_phase))*(1020000.0/250000.0)*1000.0
@@ -85,7 +141,10 @@ for file_path in file_paths:
     # plt.gca().xaxis.set_major_locator(HourLocator(interval=1))  # Major ticks every hour
     # plt.gca().xaxis.set_minor_locator(MinuteLocator(interval=15))  # Minor ticks every 15 minutes
     
-    # plt.fill_between(dates, avg_phase-std_phase, avg_phase+std_phase, facecolor='blue', alpha=0.5)
-    # plt.grid(which='both', axis='x')  # Show both major and minor grid lines
+    plt.fill_between(x, np.rad2deg(means-stds), np.rad2deg(means+stds), facecolor='blue', alpha=0.5)
+    plt.plot(x, np.rad2deg(means))
+    plt.grid(which='both', axis='x')  # Show both major and minor grid lines
+    plt.xlabel("Time (in seconds)")
+    plt.title(np.rad2deg(circstd(means)))
 
-    # plt.show()
+    plt.show()
