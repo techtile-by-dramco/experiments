6e152b2aa45a4bb9852ecbbeec03a26c177b66ab
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-02-27 14:37:40 +0100

    Update usrp-cal.py

--- 00_calibration/usrp-cal.py
+++ 00_calibration/usrp-cal.py
@@ -77,7 +77,7 @@ INIT_DELAY = 0.2  # 200ms initial delay before transmit
 RATE = 250e3
 CAPTURE_TIME = 5
 LOOPBACK_TX_GAIN = 70
-LOOPBACK_RX_GAIN = 30
+LOOPBACK_RX_GAIN = 25
 REF_RX_GAIN = 32 # emperical determined
 
 
