36525f683e8e5414b7e840ca90a15752bf9670ab
Jarne Van Mulders <33618569+jarnevanmulders@users.noreply.github.com>
2024-02-28 15:02:56 +0100

    Update plots

--- 01_distributed_non_coherent_beamforming/plot/plot_meas_data.py
+++ 01_distributed_non_coherent_beamforming/plot/plot_meas_data.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import os
 import numpy as np
+from TechtilePlotter.TechtilePlotter import TechtilePlotter
 
 # Get the absolute path of the currently executing Python script
 current_script_path = os.path.abspath(__file__)
@@ -24,35 +25,43 @@ df = df.round(decimals=3)
 
 print(np.mean(df.dbm))
 
-# Create a layout with a specified aspect ratio
-layout = go.Layout(
-    scene=dict(
-        aspectmode='manual',
-        aspectratio=dict(x=2, y=1, z=1)  # Adjust x and y ratio to make it rectangular
-    )
-)
+plt = TechtilePlotter(f"RX power plot in [dBm] with TX gain {df.tx_gain[0]}")
+#plt.antennas()
+plt.measurements(df['x'], df['y'], df['z'], df['dbm'])
 
-fig = go.Figure(data=go.Scatter3d(
-    x=df['x'],
-    y=df['y'],
-    z=df['z'],
-    text=df['dbm'],
-    mode='markers',
-    marker=dict(color=df.dbm,colorscale='Viridis', size=14, colorbar=dict(thickness=20))
-), layout=layout)
+# # Create a layout with a specified aspect ratio
+# layout = go.Layout(
+#     scene=dict(
+#         aspectmode='manual',
+#         aspectratio=dict(x=2, y=1, z=1)  # Adjust x and y ratio to make it rectangular
+#     )
+# )
 
-# Define fixed dimensions for the axes
-fig.update_layout(
-    title=f"RX power plot in [dBm] with TX gain {df.tx_gain[0]}",
-    scene=dict(
-        xaxis=dict(range=[0, 8.4]),  # Set the range for the x-axis
-        yaxis=dict(range=[0, 4]),  # Set the range for the y-axis
-        zaxis=dict(range=[0, 2.4])   # Set the range for the z-axis
-    )
+# fig = go.Figure(data=go.Scatter3d(
+#     x=df['x'],
+#     y=df['y'],
+#     z=df['z'],
+#     text=df['dbm'],
+#     mode='markers',
+#     marker=dict(color=df.dbm,colorscale='Viridis', size=14, colorbar=dict(thickness=20))
+# ), layout=layout)
 
-)
+# # Define fixed dimensions for the axes
+# fig.update_layout(
+#     title=f"RX power plot in [dBm] with TX gain {df.tx_gain[0]}",
+# )
+# fig.update_layout(
+#     scene=dict(
+#         xaxis=dict(range=[0, 8.4]),  # Set the range for the x-axis
+#         yaxis=dict(range=[0, 4]),  # Set the range for the y-axis
+#         zaxis=dict(range=[0, 2.4])   # Set the range for the z-axis
+#     )
+
+# )
+
+
 
 # fig.show()
 
 # Save to html
-fig.write_html(f"{os.path.dirname(current_script_path)}/{nr}_gain_{df.tx_gain[0]}.html")
+plt.html(f"{os.path.dirname(current_script_path)}/{nr}_gain_{df.tx_gain[0]}.html")
