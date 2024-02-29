662b1cfeeac6ce57019dcc0d56384b3e5f5b72f0
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-02-29 14:52:43 +0100

    Create sync-server.py

---
+++ 00_calibration/sync-server.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# simple_pub.py
+import zmq
+import time
+from datetime import datetime, timezone
+
+
+host = "*"
+port = "5557"
+
+# Creates a socket instance
+context = zmq.Context()
+
+
+start_socket = context.socket(zmq.PUB)
+# Binds the socket to a predefined port on localhost
+start_socket.bind("tcp://{}:{}".format(host, port))
+
+start_socket.send_string("SYNC")
+print("SYNC")
+
