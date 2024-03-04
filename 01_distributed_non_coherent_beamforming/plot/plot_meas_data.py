ff474c6d0a39cb885c5326d85528d10df3336e1f
Jarne Van Mulders <Jarne.vanmulders@kuleuven.be>
2024-03-04 11:36:45 +0100

    Update 01 experiment html files

--- 01_distributed_non_coherent_beamforming/plot/plot_meas_data.py
+++ 01_distributed_non_coherent_beamforming/plot/plot_meas_data.py
@@ -11,11 +11,15 @@ current_script_path = os.path.abspath(__file__)
 
 # TODO --> average dbm values for same position data
 
+linux = "/home"
+windows = "C:/Users"
+
+init_path = windows
 user_name = "/jarne"
 path_to_repo = "/Documents/GitHub"
-nr = "1709111155"
+nr = "1709112625"
 
-csv_file_path = f"/home{user_name}{path_to_repo}/experiments/01_distributed_non_coherent_beamforming/meas_data/{nr}_nc_data.csv"
+csv_file_path = f"{init_path}{user_name}{path_to_repo}/experiments/01_distributed_non_coherent_beamforming/meas_data/{nr}_nc_data.csv"
 
 # Read the CSV file
 df = pd.read_csv(csv_file_path, header=None, names=['x', 'y', 'z', 'utc', 'dbm', 'tx_gain', 'cable_loss'])
@@ -23,45 +27,15 @@ df = pd.read_csv(csv_file_path, header=None, names=['x', 'y', 'z', 'utc', 'dbm',
 # Round data
 df = df.round(decimals=3)
 
+# Retrieve the mean of the measured values
 print(np.mean(df.dbm))
 
 plt = TechtilePlotter(f"RX power plot in [dBm] with TX gain {df.tx_gain[0]}")
-#plt.antennas()
-plt.measurements(df['x'], df['y'], df['z'], df['dbm'])
+plt.antennas()
+plt.measurements(df['x'], df['y'], df['z'], df['dbm'], label = df['dbm'])
 
-# # Create a layout with a specified aspect ratio
-# layout = go.Layout(
-#     scene=dict(
-#         aspectmode='manual',
-#         aspectratio=dict(x=2, y=1, z=1)  # Adjust x and y ratio to make it rectangular
-#     )
-# )
+# Show the plot
+# plt.show()
 
-# fig = go.Figure(data=go.Scatter3d(
-#     x=df['x'],
-#     y=df['y'],
-#     z=df['z'],
-#     text=df['dbm'],
-#     mode='markers',
-#     marker=dict(color=df.dbm,colorscale='Viridis', size=14, colorbar=dict(thickness=20))
-# ), layout=layout)
-
-# # Define fixed dimensions for the axes
-# fig.update_layout(
-#     title=f"RX power plot in [dBm] with TX gain {df.tx_gain[0]}",
-# )
-# fig.update_layout(
-#     scene=dict(
-#         xaxis=dict(range=[0, 8.4]),  # Set the range for the x-axis
-#         yaxis=dict(range=[0, 4]),  # Set the range for the y-axis
-#         zaxis=dict(range=[0, 2.4])   # Set the range for the z-axis
-#     )
-
-# )
-
-
-
-# fig.show()
-
-# Save to html
+# Save the plot to html
 plt.html(f"{os.path.dirname(current_script_path)}/{nr}_gain_{df.tx_gain[0]}.html")
