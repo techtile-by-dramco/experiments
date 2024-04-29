7c4d10a9bb9bed0c93a0595bc50369eb330344bd
Jarne Van Mulders <Jarne.vanmulders@kuleuven.be>
2024-04-29 17:10:01 +0200

    Update zmq-test.py

--- 04_backscatter_communication/2_2_experiment_4b_part_2/zmq-test.py
+++ 04_backscatter_communication/2_2_experiment_4b_part_2/zmq-test.py
@@ -11,13 +11,22 @@ context = zmq.Context()
 socket = context.socket(zmq.PUB)
 socket.bind("tcp://0.0.0.0:5555")  # Bind to local address and port
 
+status_now = 'capture_bs_start'
+
 while 1:
 
     file_timestamp = round(time.time())
 
-    data = dict(name=file_timestamp)
+    if status_now == 'capture_bs_start':
+        status_now = 'capture_bs_stop'
+    else:
+        status_now = 'capture_bs_start'
 
-    print(file_timestamp)
+    data = dict(name=file_timestamp,
+                status=status_now,
+                )
+
+    print(data)
 
     # Send JSON data over the socket
     socket.send_string(json.dumps(data))
