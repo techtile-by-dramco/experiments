3fd5cb36905eb42b45b48a213031609609bc6323
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-05-14 14:27:01 +0200

    Update usrp-cal.py

--- 00_calibration/usrp-cal.py
+++ 00_calibration/usrp-cal.py
@@ -635,8 +635,9 @@ def get_current_time(usrp):
 
 
 def main():
-    # "mode_n=integer" # "fpga=/home/pi/experiments/00_calibration/usrp_b210_fpga_loopback.bin"
-    usrp = uhd.usrp.MultiUSRP()
+    # "mode_n=integer" # 
+    usrp = uhd.usrp.MultiUSRP(
+        "fpga=/home/pi/experiments/00_calibration/usrp_b210_fpga_loopback.bin")
     logger.info("Using Device: %s", usrp.get_pp_string())
     tx_streamer, rx_streamer = setup(usrp, server_ip)
 
