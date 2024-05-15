ce3bb9d0bdbcfe66839c38880d246134d3e25d63
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-05-15 15:44:20 +0200

    Update sync-server.py

--- 00_calibration/sync-server.py
+++ 00_calibration/sync-server.py
@@ -19,17 +19,19 @@ start_socket = context.socket(zmq.PUB)
 # Binds the socket to a predefined port on localhost
 start_socket.bind("tcp://{}:{}".format(host, port))
 
-try:
-    input(f"Press Enter to send 'SYNC' message with delay {delay}s...")
-except KeyboardInterrupt:
-    print("Exiting...")
-    sys.exit()
+while 1:
 
-time.sleep(delay)
+    try:
+        input(f"Press Enter to send 'SYNC' message with delay {delay}s...")
+    except KeyboardInterrupt:
+        print("Exiting...")
+        sys.exit()
 
-try:
-    start_socket.send_string("SYNC")
-    print("SYNC")
-except KeyboardInterrupt:
-    print("Exiting...")
-    sys.exit()
+    time.sleep(delay)
+
+    try:
+        start_socket.send_string("SYNC")
+        print("SYNC")
+    except KeyboardInterrupt:
+        print("Exiting...")
+        sys.exit()
