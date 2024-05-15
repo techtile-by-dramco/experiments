33a32f0f63ad7442347dbf078e07596b7c5f24a1
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-05-15 13:18:22 +0200

    Update sync-server.py

--- 00_calibration/sync-server.py
+++ 00_calibration/sync-server.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
-# simple_pub.py
 import zmq
 import time
-from datetime import datetime, timezone
+import sys
 
+if len(sys.argv) > 1:
+    delay = int(sys.argv[1])
+else:
+    delay = 10
 
 host = "*"
 port = "5557"
@@ -12,13 +15,21 @@ port = "5557"
 # Creates a socket instance
 context = zmq.Context()
 
-
 start_socket = context.socket(zmq.PUB)
 # Binds the socket to a predefined port on localhost
 start_socket.bind("tcp://{}:{}".format(host, port))
 
-time.sleep(10)
+try:
+    input(f"Press Enter to send 'SYNC' message with delay {delay}s...")
+except KeyboardInterrupt:
+    print("Exiting...")
+    sys.exit()
 
-start_socket.send_string("SYNC")
-print("SYNC")
+time.sleep(delay)
 
+try:
+    start_socket.send_string("SYNC")
+    print("SYNC")
+except KeyboardInterrupt:
+    print("Exiting...")
+    sys.exit()
