98ba909aa9e1fff411ddde6a17ca3202ea3cf157
Jarne Van Mulders <Jarne.vanmulders@kuleuven.be>
2024-05-16 16:58:17 +0200

    Add exp. controller folder

---
+++ 99_experiment_controller/ZMQ_controller.py
@@ -0,0 +1,29 @@
+import zmq
+
+def main():
+    context = zmq.Context()
+    publisher = context.socket(zmq.PUB)
+    publisher.bind("tcp://*:5558")
+
+    while True:
+        # Read user input
+        user_input = input("Enter 'init', 'start', 'stop', or 'close' to stop the server: ").strip().lower()
+
+        # Check if input is valid
+        if user_input not in ["init", "start", "stop", "close"]:
+            print("Invalid input. Please enter 'start', 'stop', or 'close'.")
+            continue
+
+        # Publish the input on the "phase" topic
+        publisher.send_multipart([b"phase", user_input.encode()])
+
+        # If user input is 'close', break out of the loop and stop the server
+        if user_input == "close":
+            break
+
+    # Close the socket and context when done
+    publisher.close()
+    context.term()
+
+if __name__ == "__main__":
+    main()
