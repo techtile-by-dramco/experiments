faa860f81647721c49f2154150993e1f3c39d606
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-05-21 13:40:30 +0200

    Update meas-phase-scope.py

--- 00_calibration/002_repeated_setup/meas-phase-scope.py
+++ 00_calibration/002_repeated_setup/meas-phase-scope.py
@@ -1,42 +1,71 @@
 import time  # std module
 import pyvisa as visa  # http://github.com/hgrecco/pyvisa
 import numpy as np  # http://www.numpy.org/
-from scipy.signal import find_peaks
+from scipy.stats import circmean
 
 from enum import Enum
 
 ip = "192.108.0.251"
 
 rm = visa.ResourceManager()
-scope = rm.open_resource(f'TCPIP::{ip}::INSTR')
+scope = rm.open_resource(f'TCPIP::{ip}::4000::SOCKET', write_termination='\n', read_termination='\n')
+print(scope.query('*IDN?'))
+
+
+def get_last_meas_name() -> str:
+    return scope.query("MEASUrement:LIST?").replace('\n', '').split(',')[-1]
 
 # Add new measurement phase
 scope.write("MEASUREMENT:ADDMEAS PHASE")
 
-# Query list of measurements
-meas_list = scope.query("MEASUrement:LIST?")
-meas_list = meas_list.replace('\n', '').split(',')
+
+# Define measurement number
+meas_name = get_last_meas_name().strip()
+print(f"Reading {meas_name}")
+
 
 # Define channels (phase measurment)
 channel_1 = "CH1"
 channel_2 = "CH2"
 
 # Update sources from last added measurement
-scope.write(f"MEASUrement:{meas_list[-1]}:SOUrce1 {channel_1}")
-scope.write(f"MEASUrement:{meas_list[-1]}:SOUrce2 {channel_2}")
+scope.write(f"MEASUrement:{meas_name}:SOUrce1 {channel_1}")
+scope.write(f"MEASUrement:{meas_name}:SOUrce2 {channel_2}")
 
 
-def get_last_meas_name():
-  return scope.query("MEASUrement:LIST?").replace('\n', '').split(',')[-1]
+
+meas_ongoing = False
+
+meas_data = []
+
+num_valid_in_meas = 0
+
 
 
 while 1:
 
-  # Define measurement number
-  # meas_name = 'MEAS2'
-  meas_name = get_last_meas_name()
+    res = scope.query(f"MEASUrement:{meas_name}:RESUlts:HISTory:MEAN?")
 
-  res = scope.query(f"MEASUrement:{meas_name}:RESUlts:HISTory:MEAN?")
-  print(f"{meas_name}: {res}")
+    res_valid = float(res) < 360.0
 
-  time.sleep(1)
+    # TODO ignore 1 shots
+
+    if res_valid and not np.isnan(res_valid):
+        print(".", end="", flush=True)
+        meas_data.append(float(res))
+        num_valid_in_meas += 1
+        if not meas_ongoing:
+            meas_ongoing = True
+
+    if not res_valid and meas_ongoing:
+        # stop previous measurements
+        print()
+        meas_ongoing = False
+        if num_valid_in_meas > 20:
+            # skip the last and first 5 meas
+            print(np.rad2deg(circmean(np.deg2rad(meas_data[5:-5]))))
+        meas_data = []
+        num_valid_in_meas = 0
+    
+    time.sleep(1)
+
