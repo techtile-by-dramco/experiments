332fcf16964f4e1148b9a7da044c16db84afbdc4
Gilles Callebaut, PhD <callebaut.gilles@gmail.com>
2024-05-27 09:39:25 +0200

    Delete 00_calibration/003_repeated_setup/start-exp.sh

--- 00_calibration/003_repeated_setup/start-exp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-gnome-terminal -- /bin/sh -c 'sshpass -p LookVogelDomPot229 ssh techtile@192.108.0.1 "ssh pi@rpi-t02.local && ls && exec bash"; exec bash'
-# gnome-terminal -e -- "bash -c 'sshpass -p LookVogelDomPot229 ssh techtile@192.108.0.1' && ssh pi@rpi-t02.local" | gnome-terminal -e "bash -c 'sshpass -p LookVogelDomPot229 ssh techtile@192.108.0.1' && ssh pi@rpi-t03.local" |  gnome-terminal -e "bash -c 'sshpass -p LookVogelDomPot229 ssh techtile@192.108.0.1' && ssh pi@rpi-t04.local"
-
-
-sshpass -p LookVogelDomPot229 ssh techtile@192.108.0.1
-ssh pi@rpi-t04.local
-cd experiments/00_calibration/002_repeated_setup/
