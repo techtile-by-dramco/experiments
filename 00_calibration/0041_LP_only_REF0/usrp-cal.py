b48d6170545c538306d38a7085e960f0dc2d3df2
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-05-29 12:05:00 +0200

    Update usrp-cal.py

--- 00_calibration/0041_LP_only_REF0/usrp-cal.py
+++ 00_calibration/0041_LP_only_REF0/usrp-cal.py
@@ -402,7 +402,7 @@ def setup(usrp):
     
     usrp.set_rx_gain(LOOPBACK_RX_GAIN, LOOPBACK_RX_CH)
     usrp.set_rx_gain(REF_RX_GAIN, REF_RX_CH)
-    usrp.set_rx_antenna(usrp.get_rx_antennas(LOOPBACK_RX_CH)[0], LOOPBACK_RX_CH)
+    usrp.set_rx_antenna('RX', LOOPBACK_RX_CH)
 
     logger.debug("Setting TX")
     # specific settings from loopback/REF PLL
