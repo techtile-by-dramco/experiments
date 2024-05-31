7ebe506d18a564ba2a3160c7a78fcfa1fda7d80a
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-05-31 13:53:54 +0200

    update 0041

--- 00_calibration/0041_LP_only_REF0/get-files-from-rpis.sh
+++ 00_calibration/0041_LP_only_REF0/get-files-from-rpis.sh
@@ -1,7 +1,10 @@
-ssh techtile@192.108.0.1 -t "mkdir -p /storage/gilles/0041_LP_only_REF0/"
+read -p "Enter password: " password
 
-ssh techtile@192.108.0.1 -t "scp pi@rpi-t03.local:~/experiments/00_calibration/0041_LP_only_REF0/data_T03_* /storage/gilles/0041_LP_only_REF0/"
-ssh techtile@192.108.0.1 -t "scp pi@rpi-t04.local:~/experiments/00_calibration/0041_LP_only_REF0/data_T04_* /storage/gilles/0041_LP_only_REF0/"
-ssh techtile@192.108.0.1 -t "scp pi@rpi-t01.local:~/experiments/00_calibration/0041_LP_only_REF0/data_T01_* /storage/gilles/0041_LP_only_REF0/"
 
-scp techtile@192.108.0.1:/storage/gilles/0041_LP_only_REF0/data_T* .
+sshpass -p "$password"  ssh techtile@192.108.0.1 -t "mkdir -p /storage/gilles/0041_LP_only_REF0/"
+
+sshpass -p "$password"  ssh techtile@192.108.0.1 -t "scp pi@rpi-t03.local:~/experiments/00_calibration/0041_LP_only_REF0/data_T03_* /storage/gilles/0041_LP_only_REF0/"
+sshpass -p "$password"  ssh techtile@192.108.0.1 -t "scp pi@rpi-t04.local:~/experiments/00_calibration/0041_LP_only_REF0/data_T04_* /storage/gilles/0041_LP_only_REF0/"
+sshpass -p "$password"  ssh techtile@192.108.0.1 -t "scp pi@rpi-t01.local:~/experiments/00_calibration/0041_LP_only_REF0/data_T01_* /storage/gilles/0041_LP_only_REF0/"
+
+sshpass -p "$password"  scp techtile@192.108.0.1:/storage/gilles/0041_LP_only_REF0/data_T* .
