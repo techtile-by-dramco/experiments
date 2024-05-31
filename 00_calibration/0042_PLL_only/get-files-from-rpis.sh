d8ce47af6edd14c74e10d351038c2182dc559209
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-05-31 15:17:16 +0200

    new meas

---
+++ 00_calibration/0042_PLL_only/get-files-from-rpis.sh
@@ -0,0 +1,11 @@
+read -p "Enter password: " password
+
+
+sshpass -p "$password"  ssh techtile@192.108.0.1 -t "mkdir -p /storage/gilles/0042_PLL_only/"
+
+sshpass -p "$password"  ssh techtile@192.108.0.1 -t "scp pi@rpi-t03.local:~/experiments/00_calibration/0042_PLL_only/data_T03_* /storage/gilles/0042_PLL_only/"
+sshpass -p "$password"  ssh techtile@192.108.0.1 -t "scp pi@rpi-t04.local:~/experiments/00_calibration/0042_PLL_only/data_T04_* /storage/gilles/0042_PLL_only/"
+sshpass -p "$password"  ssh techtile@192.108.0.1 -t "scp pi@rpi-t01.local:~/experiments/00_calibration/0042_PLL_only/data_T01_* /storage/gilles/0042_PLL_only/"
+sshpass -p "$password"  ssh techtile@192.108.0.1 -t "scp pi@rpi-t02.local:~/experiments/00_calibration/0042_PLL_only/data_T02_* /storage/gilles/0042_PLL_only/"
+
+sshpass -p "$password"  scp techtile@192.108.0.1:/storage/gilles/0042_PLL_only/data_T* .
