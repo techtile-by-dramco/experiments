12bbe1f7dbbe0c5e7fa9211f217a737c7eee6cde
Jarne Van Mulders <33618569+jarnevanmulders@users.noreply.github.com>
2024-08-01 15:54:05 +0200

    update scripts

--- 01_distributed_non_coherent_beamforming/reindeer-experiments-fast-sampling/plot/plot-results.py
+++ 01_distributed_non_coherent_beamforming/reindeer-experiments-fast-sampling/plot/plot-results.py
@@ -19,14 +19,15 @@ from yaml_utils import *
 
 file_path = None
 
-path_name = "one_tone_phase_duration_5_m1"
+path_name = 'multi_tone_m1'
+#path_name = "one_tone_phase_duration_5_m1"
 
 x1 = []
 x2 = []
 y = []
 
-begin_no_meas = 0
-no_meas = 1
+begin_no_meas = 7
+no_meas = 8
 
 saved_data_names = ["ep", "scope"]
 
@@ -66,28 +67,49 @@ for i in range(begin_no_meas, no_meas):
         # gain = config.get('client', {}).get('hosts', {}).get('all', {}).get('gain', {})
         # duration = config.get('client', {}).get('hosts', {}).get('all', {}).get('duration', {})
 
-        # Extract 'utc' and 'pwr_nw' columns
+        # Extract 'utc' and 'pwr_pw' columns
         #utc = df['utc']
-        pwr_nw = df[0]['pwr_nw']
+        pwr_pw = df[0]['pwr_pw']
         buffer_voltage_mv = df[0]['buffer_voltage_mv']
         res = df[0]['resistance']
         #dbm = df['dbm']
 
         # Check race condition faults
-        plt.plot(df[0]["timestamp"], pwr_nw/1e3 - buffer_voltage_mv**2/res)
+        #plt.plot(df[0]["timestamp"], pwr_pw/1e6 - buffer_voltage_mv**2/res)
 
         print(f"Max voltage: {max(buffer_voltage_mv)}")
 
-        time = (df[0]["timestamp"].values - start_time)/1e3
-
         pwr_dbm = df[1]['dbm'] + 10
-        time2 = (df[1]["timestamp"].values - start_time)/1e3
 
-        dc = np.mean(pwr_nw/1e3)
+        # Define list of timestamps
+        time_ep = (df[0]["timestamp"].values - start_time)/1e3
+        time_scope = (df[1]["timestamp"].values - start_time)/1e3
+
+        # Find out time when USRP transmitters became active
+        usrp_active_index = np.where(pwr_dbm > -70)[0][0]
+        usrp_active_time = time_scope[usrp_active_index]
+
+        # Update lists of data scope
+        time_scope = (time_scope - usrp_active_time)[usrp_active_index:]
+        pwr_dbm = pwr_dbm[usrp_active_index:]
+        
+        # Update EP time data
+        time_ep = (time_ep - usrp_active_time)
+
+        # Find similarly index for ep
+        usrp_active_index_ep = np.where(time_ep > time_scope[0])[0][0]
+        print(usrp_active_index_ep)
+
+        # Update lists of ep data
+        time_ep = time_ep[usrp_active_index_ep:]
+        pwr_pw = pwr_pw[usrp_active_index_ep:]
+        buffer_voltage_mv = buffer_voltage_mv[usrp_active_index_ep:]
+        res = res[usrp_active_index_ep:]
+
+        dc = np.mean(pwr_pw/1e6)
         #rf = np.mean(10**(dbm/10)*1e3)
 
-
-        print(f"DC mean {np.mean(pwr_nw/1e3)}")
+        print(f"DC mean {np.mean(pwr_pw/1e6)}")
         print(f"RF mean {np.mean(10**(pwr_dbm/10)*1e3)}")
         print(f"RF mean {np.max(pwr_dbm)}")
 
@@ -95,16 +117,16 @@ for i in range(begin_no_meas, no_meas):
         #x2.append(rf)
 
 
-        # Plot 'utc' against 'pwr_nw'
+        # Plot 'utc' against 'pwr_pw'
         plt.figure(figsize=(10, 6))
-        plt.plot(time, pwr_nw/1e3, label = 'DC power (EP profiler)')
-        plt.plot(time, buffer_voltage_mv/1e3, label = 'Voltage (EP profiler)')
-        plt.plot(time, res/1e6, label = 'Resistance (EP profiler)')
-        plt.plot(time2, 10**(pwr_dbm/10)*1e3, label = 'RF power (FFT scope)')
-        # plt.plot(x, 10*np.log(pwr_nw/1e6), marker='o', label='harvester DC power')
+        plt.plot(time_ep, pwr_pw/1e6, label = 'DC power (EP profiler)')
+        plt.plot(time_ep, buffer_voltage_mv/1e3, label = 'Voltage (EP profiler)')
+        plt.plot(time_ep, res/1e6, label = 'Resistance (EP profiler)')
+        plt.plot(time_scope, 10**(pwr_dbm/10)*1e3, label = 'RF power (FFT scope)')
+        # plt.plot(x, 10*np.log(pwr_pw/1e6), marker='o', label='harvester DC power')
         # plt.plot(x, dbm, marker='o', label='RF power')
         # plt.ylabel('Power (dBm)')
-        # plt.plot(time, pwr_nw/1e3, marker='o', label='harvester DC power')
+        # plt.plot(time, pwr_pw/1e3, marker='o', label='harvester DC power')
         # plt.plot(time, 10**(dbm/10)*1e3, marker='o', label='harvested RF power')
         # plt.plot(x, buffer_voltage_mv, marker='o', label='harvester DC voltage')dd
         plt.xlabel('Measurement time [s]')
