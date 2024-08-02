52855e8fca56c60779288c3922ba00758e7ca7f3
Jarne Van Mulders <33618569+jarnevanmulders@users.noreply.github.com>
2024-08-02 09:56:38 +0200

    Add meas data

--- 01_distributed_non_coherent_beamforming/reindeer-experiments-fast-sampling/meas/meas_multi_vs_single.py
+++ 01_distributed_non_coherent_beamforming/reindeer-experiments-fast-sampling/meas/meas_multi_vs_single.py
@@ -14,40 +14,40 @@ from yaml_utils import *
 
 number_of_measurements_per_config = 11
 
-# Change frequencies settings in config.yaml to multi tone
-script_path = f"{exp_dir}/meas/config_signal_args.py"
-subprocess.run(['python3', script_path, '--frequency', '920E6', '--offset', '1E2', '--multitone'], capture_output=False, text=True)
+# # Change frequencies settings in config.yaml to multi tone
+# script_path = f"{exp_dir}/meas/config_signal_args.py"
+# subprocess.run(['python3', script_path, '--frequency', '920E6', '--offset', '1E2', '--multitone'], capture_output=False, text=True)
 
-for i in range(number_of_measurements_per_config):
+# for i in range(number_of_measurements_per_config):
 
-    #   Read YAML file
-    config = read_yaml_file(f"{exp_dir}/config.yaml")
+#     #   Read YAML file
+#     config = read_yaml_file(f"{exp_dir}/config.yaml")
 
-    config['info']['description'] = "Measurement multi tone signals"
-    config['info']['exp_name'] = f"phase_{75+i}" 
-    config['info']['data_save_path'] = "data/multi_tone_m1/"
-    config['client']['hosts']['all']['gain'] = 75 + i 
-    config['client']['hosts']['all']['duration'] = 3600 # high enough that phases not change
-    config['control']['duration'] = 60*30
+#     config['info']['description'] = "Measurement multi tone signals"
+#     config['info']['exp_name'] = f"phase_{75+i}" 
+#     config['info']['data_save_path'] = "data/multi_tone_m1/"
+#     config['client']['hosts']['all']['gain'] = 75 + i 
+#     config['client']['hosts']['all']['duration'] = 3600 # high enough that phases not change
+#     config['control']['duration'] = 60*30
 
-    # Write the updated YAML content back to the file
-    with open(f"{exp_dir}/config.yaml", 'w') as file:
-        yaml.safe_dump(config, file)
+#     # Write the updated YAML content back to the file
+#     with open(f"{exp_dir}/config.yaml", 'w') as file:
+#         yaml.safe_dump(config, file)
 
-    # Path to the second script
-    script_path = f"{exp_dir}/server/main.py"
+#     # Path to the second script
+#     script_path = f"{exp_dir}/server/main.py"
 
-    # Run the second script
-    result = subprocess.run(['python3', script_path], capture_output=False, text=True)
+#     # Run the second script
+#     result = subprocess.run(['python3', script_path], capture_output=False, text=True)
 
-    print(f"Measurement {i}: DONE")
+#     print(f"Measurement {i}: DONE")
 
 
 # Change frequencies settings in config.yaml to single tone
 script_path = f"{exp_dir}/meas/config_signal_args.py"
 subprocess.run(['python3', script_path, '--frequency', '920E6', '--singletone'], capture_output=False, text=True)
 
-for i in range(number_of_measurements_per_config):
+for i in range(1, number_of_measurements_per_config): # <<<<<<<<<<<<<<<<<------------ *** 1 *** WEG DOEN
 
     #   Read YAML file
     config = read_yaml_file(f"{exp_dir}/config.yaml")
