7436bdf1743ff49da71fe75ff534c1be44487035
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-08-19 14:43:35 +0200

    update 007

---
+++ 00_calibration/007_receive_no_compensation/get-files-from-rpis.sh
@@ -0,0 +1,11 @@
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+read -p "Enter password: " password
+
+
+sshpass -p "$password" ssh techtile@192.108.0.1 -t "mkdir -p /storage/gilles/$SCRIPT_DIR/"
+
+sshpass -p "$password" ssh techtile@192.108.0.1 -t "scp pi@rpi-t01.local:~/experiments/00_calibration/$SCRIPT_DIR/data_T01_* /storage/gilles/$SCRIPT_DIR/"
+sshpass -p "$password" ssh techtile@192.108.0.1 -t "scp pi@rpi-t04.local:~/experiments/00_calibration/$SCRIPT_DIR/data_T04_* /storage/gilles/$SCRIPT_DIR/"
+
+sshpass -p "$password" scp techtile@192.108.0.1:/storage/gilles/$SCRIPT_DIR/data_T* .
