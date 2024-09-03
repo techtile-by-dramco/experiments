e813fc41748cb22a7f3463141254cf18c0d73800
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-09-03 13:38:34 +0200

    Update usrp-pilot.py

--- 00_calibration/010_with_pilot/usrp-pilot.py
+++ 00_calibration/010_with_pilot/usrp-pilot.py
@@ -143,7 +143,7 @@ data_file = None
 
 def store_phase():
     # data_file.write(str(meas_id))
-    data_file.write(str(meas_id) + ";" + tx_phase + "\n")
+    data_file.write(str(meas_id) + ";" + str(tx_phase) + "\n")
     data_file.flush()
 
 
