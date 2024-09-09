e45c17c885827f517dcd6fc8d7ab3566dff691d5
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-09-09 15:05:45 +0200

    Update usrp-cal.sh

--- 00_calibration/011_dual_channel_rx_offset/usrp-cal.sh
+++ 00_calibration/011_dual_channel_rx_offset/usrp-cal.sh
@@ -12,19 +12,19 @@ while true; do
     echo "your script encountered an error."
     # Optionally, you can add a break or continue statement here to handle errors
   fi
-  # Increment the loop counter
-  counter=$((counter + 1))
+  
 
   # Every 10 loops, decrement the gain by 1dB
   if [ $((counter % 2)) -eq 0 ]; then
     gain=$((gain - 1))
   fi
 
+  # Increment the loop counter
+  counter=$((counter + 1))
+
    # Check if phase has reached 360 degrees
-  if [ $gain -eq 0 ]; then
+  if [ $gain -eq -1 ]; then
     echo "Gain has reached 0dB. Stopping the script."
     break
   fi
-
-  sleep 0.1 # wait for 0.1 second before running the script again
 done
