16a37bc9f553e3ecfd8e1f1ab5348a3e20d1ac9d
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-09-12 14:22:39 +0200

    Update usrp-cal.sh

--- 00_calibration/011_dual_channel_rx_offset/usrp-cal.sh
+++ 00_calibration/011_dual_channel_rx_offset/usrp-cal.sh
@@ -5,19 +5,23 @@ gain=76
 
 unique_id=$(date -u +"%Y%m%d%H%M%S")
 
+
+HOSTNAME=$(hostname)
+HOSTNAME=${HOSTNAME:4}  # Slice the hostname starting from the 5th character
+echo $HOSTNAME
+
+
 while true; do
   echo "starting with gain $gain"
+  echo "$counter,$gain,$gain" >> "data_config_${HOSTNAME}_${unique_id}.csv"
   python3 usrp-cal.py --meas $counter --gain $gain --exp $unique_id
   if [ $? -ne 0 ]; then
     echo "your script encountered an error."
     # Optionally, you can add a break or continue statement here to handle errors
   fi
   
+  gain=$((gain - 1))
 
-  # Every 10 loops, decrement the gain by 1dB
-  if [ $((counter % 2)) -eq 0 ]; then
-    gain=$((gain - 1))
-  fi
 
   # Increment the loop counter
   counter=$((counter + 1))
