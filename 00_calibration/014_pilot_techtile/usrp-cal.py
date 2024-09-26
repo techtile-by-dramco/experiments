7a32eb68323fa9fbffca23fa3a4819e2af1e998a
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-09-26 12:55:19 +0200

    Update usrp-cal.py

--- 00_calibration/014_pilot_techtile/usrp-cal.py
+++ 00_calibration/014_pilot_techtile/usrp-cal.py
@@ -489,7 +489,10 @@ def measure_pilot(usrp, rx_streamer, quit_event, result_queue, at_time=None):
 
     try:
         user_settings = usrp.get_user_settings_iface(1)
-        usrp.set_user_register(0, 0x00000003)
+        if user_settings:
+            user_settings.poke32(0,0x00000003)
+        else:
+            logger.error(" Cannot write to user settings.")
     except Exception as e:
         logger.error(e)
 
