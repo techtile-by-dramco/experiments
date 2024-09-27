3e73fa000b0b4d53da5cdb45c84ce568a104169d
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-09-27 10:44:23 +0200

    Update usrp-cal.sh

--- 00_calibration/014_pilot_techtile/usrp-cal.sh
+++ 00_calibration/014_pilot_techtile/usrp-cal.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 git pull
-counter=1
-gain=76
-
-unique_id=$(date -u +"%Y%m%d%H%M%S")
 
 while true; do
   python3 usrp-cal.py
@@ -11,19 +7,6 @@ while true; do
     echo "your script encountered an error."
     # Optionally, you can add a break or continue statement here to handle errors
   fi
-  
 
-  # Every 10 loops, decrement the gain by 1dB
-  if [ $((counter % 2)) -eq 0 ]; then
-    gain=$((gain - 1))
-  fi
-
-  # Increment the loop counter
-  counter=$((counter + 1))
-
-   # Check if phase has reached 360 degrees
-  if [ $gain -eq -1 ]; then
-    echo "Gain has reached 0dB. Stopping the script."
-    break
-  fi
+  sleep 1 # give it some time
 done
