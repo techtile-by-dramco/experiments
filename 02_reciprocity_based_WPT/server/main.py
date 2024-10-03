3dad72171cf10b3e66e407c4b51f1cd3ee71fb4f
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-10-03 16:41:31 +0200

    update meas

--- 02_reciprocity_based_WPT/server/main.py
+++ 02_reciprocity_based_WPT/server/main.py
@@ -1,6 +1,3 @@
-# /bin/python3 -m experiments.01_distributed_non_coherent_beamforming.main
-# see for relative import: https://stackoverflow.com/a/68315950/3590700
-
 # ****************************************************************************************** #
 #                                       IMPORTS / PATHS                                      #
 # ****************************************************************************************** #
@@ -19,32 +16,29 @@ server_dir = os.path.dirname(os.path.abspath(__file__))
 
 config = read_yaml_file("config.yml")
 scope = Scope(config=config["scope"])
-positioner = PositionerClient(config=config["positioning"])
+positioner = PositionerClient(config=config["positioning"], backend="zmq")
 
 positioner.start()
-
-
-current = time()
-count = 0
 
 positions = []
 values = []
 
 try:
     while True:
-        power_dBm = scope.get_power_dBm()
 
         pos = positioner.get_data()
+        power_dBm = scope.get_power_dBm()
+
         if pos is not None:
             print(power_dBm)
             positions.append(pos)
+            print(pos)
             values.append(power_dBm)
-            count +=1
         sleep(0.2)
 except KeyboardInterrupt:
     print("Ctrl+C pressed. Exiting loop and saving...")
 
     positioner.stop()
-    np.save(arr=positions, file="positions-bf")
+    np.save(arr=positions, file="positions-nobf-G19-dwars")
 
-    np.save(arr=values, file="values-bf")
+    np.save(arr=values, file="values-nobf-G19-dwars")
