3e3d9bb6723a09d0de780f296ae04068f89f9039
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-10-09 14:19:27 +0200

    Create ACRO_control_functions.py

---
+++ 02_reciprocity_based_WPT/server/ACRO_control_functions.py
@@ -0,0 +1,90 @@
+import numpy as np
+import serial
+import time
+
+
+class ACRO():
+    def __init__(self, COMport='COM5'):
+        # Establish serial connection
+        self.ser = serial.Serial(COMport, baudrate=115200, timeout=1)  # Replace with the appropriate serial port
+
+        # Wake up grbl
+        self.ser.write(b"\r\n\r\n")
+        time.sleep(2)  # Wait for grbl to initialize
+        self.ser.flushInput()  # Flush startup text in serial input
+
+
+    def wait_till_idle(self):
+        response = ""
+        while True:
+            time.sleep(0.1)
+
+            self.ser.flushInput()
+            self.ser.write(b"?\n")
+            response = self.ser.readline().decode().strip()
+            print(response)
+
+            if "Idle" in response:
+                break
+
+    def home_ACRO(self):
+        # Send the query command
+        self.ser.write(b"G54\n")
+        self.ser.write(b"?\n")
+
+        self.wait_till_idle()
+
+        # Read and decode the response
+        response = self.ser.readline().decode().strip()
+        print("Controller response:", response)
+
+        # Send homing command
+        command = f"$H\n"  # Command to start homing
+        self.ser.write(command.encode())
+        self.wait_till_idle()        # Wait until controller reports idle state
+
+        self.ser.write("G10 P0 L20 X0 Y0 Z0\n".encode())
+        self.wait_till_idle()
+
+        self.ser.write(b"G54\n")
+        self.ser.write(b"?\n")
+
+        self.wait_till_idle()
+
+        print("Machine is homed")
+
+
+    def move_ACRO(self, x, y, wait_idle=True):
+        command = f"G0 X{x} Y{y}\n"  # Command to move to specific location
+        self.ser.write(command.encode())  # Send command to move to a specific position
+        if wait_idle:
+            self.wait_till_idle()  # Wait until controller reports idle state
+
+    def move_ACRO_to_origin(self):
+        command = f"G0 X0 Y0\n"  # Command to move to specific location
+        self.ser.write(command.encode())
+        self.wait_till_idle()  # Wait until controller reports idle state
+
+
+    def close_ACRO(self):
+        self.ser.close()
+
+if __name__ == '__main__':
+    ACRO = ACRO(COMport='COM5')
+    ACRO.home_ACRO()
+
+    x = np.arange(0, 100+1, 10) + 100
+    y = np.arange(0, 100+1, 10) + 100
+
+    xx, yy = np.meshgrid(x, y)
+    xx, yy = xx.ravel(), yy.ravel()
+
+    for x, y in zip(xx, yy):
+        ACRO.move_ACRO(x, y, wait_idle=True)
+        #TODO: Perform measurement
+
+
+    # Return to origin
+    ACRO.move_ACRO_to_origin()
+
+    print("Done")
