6f63eabbb41503caf1088b08a9b3681ac202acc3
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-11-05 21:21:33 +0100

    Update record-measurement.py

--- 02_reciprocity_based_WPT/server/record-measurement.py
+++ 02_reciprocity_based_WPT/server/record-measurement.py
@@ -14,6 +14,7 @@ import numpy as np
 import zmq
 
 # Get the current directory of the script
+SAVE_EVERY = 5*60
 server_dir = os.path.dirname(os.path.abspath(__file__))
 
 config = read_yaml_file("config.yml")
@@ -56,14 +57,16 @@ def wait_till_go_from_server(ip="10.128.52.53"):
 
 
 plt = TechtilePlotter(realtime=True)
-# meas_id, unique_id = wait_till_go_from_server()
-# sleep(29.0)  # wake-up 10 seconds before rover starts to move
+meas_id, unique_id = wait_till_go_from_server()
+sleep(29.0)  # wake-up 10 seconds before rover starts to move
 
 # start to measure for XX long
 start = time()
 
 positions = []
 values = []
+
+last_save = -1
 
 while True:
 
@@ -77,8 +80,13 @@ while True:
         values.append(power_dBm)
         plt.measurements_rt(pos.x, pos.y, pos.z, power_dBm)
     sleep(0.1)
+
+    if last_save + SAVE_EVERY < time():
+        np.save(arr=positions, file=f"../data/positions-{unique_id}")
+        np.save(arr=values, file=f"../data/values-{unique_id}")
+        last_save = time()
 print("Ctrl+C pressed. Exiting loop and saving...")
-meas_name = f"bf-ceiling-grid-20241024"
-np.save(arr=positions, file=f"../data/positions-{meas_name}")
-np.save(arr=values, file=f"../data/values-{meas_name}")
+# meas_name = f"bf-ceiling-grid-20241105-70db-tx"
+np.save(arr=positions, file=f"../data/positions-{unique_id}")
+np.save(arr=values, file=f"../data/values-{unique_id}")
 positioner.stop()
